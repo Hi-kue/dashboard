1e84c803994ec6042705a16450c364cc94af738c
Muhammad Bilal Khan <lofi.audit@gmail.com>
2023-09-10 23:34:33 -0400

    Changes to Footer.svelte, Header.svelte, +layout.svelte

---
+++ src/components/Footer.svelte
@@ -0,0 +1,4 @@
+<script>
+</script>
+
+<p class="outline-dashed font-serif">&copy; {new Date().getFullYear()} Acme Inc</p>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
 	import '../app.css';
+	import Footer from '../components/Footer.svelte';
 </script>
 
+<Footer/>
 <slot />
